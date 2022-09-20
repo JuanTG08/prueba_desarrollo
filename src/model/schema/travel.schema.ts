72f120d6ceee28422f27651e6ba3e11e63efe1ae
Japplet <juantriana@seti.com.co>
2022-09-20 12:39:17 -0500

    :bricks: Se crea el esqueva escencial para el viaje

---
+++ src/model/schema/travel.schema.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+const TravelSchema = new mongoose.Schema({
+    vehicle: {
+        ref: 'Vehicle',
+        type: Schema.Types.ObjectId,
+    },
+    client: {
+        ref: 'User',
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    date_ac: { // Fecha Aceptado
+        type: String,
+    },
+    date_end: { // Fecha Finalizado
+        type: String,
+    },
+    location_init: {
+        type: Object, // Locacalizacion principal e inicial
+        required: true,
+    },
+    location_end: {
+        type: Object, // Locacalizacion final
+        required: true,
+    },
+    travel_status: {
+        type: String,
+        enum: ['wait', 'accept', 'progress', 'finalized'],
+    },
+    status: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    }
+});
+
+const Travel = mongoose.model('Travel', TravelSchema);
+export default Travel;
